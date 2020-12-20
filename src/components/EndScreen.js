e3767dc269b96d9f84755c4d75508981a4433bfc
MRFolio <roose.markus@gmail.com>
2020-12-20 23:58:58 +0200

    feat(end-screen): add result container

--- src/components/EndScreen.js
+++ src/components/EndScreen.js
@@ -1,6 +1,7 @@
 import { BsThreeDotsVertical } from 'react-icons/bs';
 import { useHistory } from 'react-router-dom';
 import { useQuizContext } from '../context/QuizContext';
+import grey from '../images/grey.png';
 import styles from './EndScreen.module.scss';
 import Modal from './Modal';
 
@@ -21,6 +22,16 @@ const EndScreen = () => {
         Your final score is <strong>{correct} </strong>out of{' '}
         <strong>{questions.length}</strong>.
       </p>
+      <article className={styles.result}>
+        <img
+          src={grey}
+          alt="background pattern greyscale"
+          className={styles.image}
+        />
+        <p className={styles.resultNumber}>Your Score</p>
+        <p className={styles.resultNumber}>Advanced</p>
+        <p className={styles.resultNumber}>100%</p>
+      </article>
       <p className={styles.resultText}>
         You answered{' '}
         <strong>{((correct / questions.length) * 100).toFixed(0)}%</strong> of
